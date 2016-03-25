ca7a81c7d62598caae851e88331965c45988d59b
Fabio <kucinskifk@gmail.com>
2016-03-25 16:58:09 -0300

    Add icone aplicação

--- src/br/univel/address/MainApp.java
+++ src/br/univel/address/MainApp.java
@@ -59,7 +59,7 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
-
+        this.primaryStage.getIcons().add(new Image("file:resources/images/Agenda.png"));
         initRootLayout();
 
         showPersonOverview();
